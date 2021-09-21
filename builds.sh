473595b474d13fcccd02d3360bf8b9877d2907b7
Michael Schmidt <Michael.Schmidt@VUMC.org>
2021-09-21 12:50:17 -0500

    remove localhost:5000 pushes

--- builds.sh
+++ builds.sh
@@ -1,8 +1,5 @@
-docker build --target build-achilles --tag localhost:5000/ohdsi-achilles .
-docker push localhost:5000/ohdsi-achilles
+docker build --target build-achilles --tag ohdsi-achilles .
 
-docker build --target build-data-quality-dashboard --tag localhost:5000/ohdsi-data-quality-dashboard .
-docker push localhost:5000/ohdsi-data-quality-dashboard
+docker build --target build-data-quality-dashboard --tag ohdsi-data-quality-dashboard .
 
-docker build --target build-cohort-diagnostics --tag localhost:5000/ohdsi-cohort-diagnostics .
-docker push localhost:5000/ohdsi-cohort-diagnostics
+docker build --target build-cohort-diagnostics --tag ohdsi-cohort-diagnostics .
